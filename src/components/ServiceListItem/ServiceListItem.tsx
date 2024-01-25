0d6b4bfa9242d9e7f70d4a11844a12dddf79a528
Martynas Hoppenas <martynas.hoppenas@gmail.com>
2024-01-25 18:51:12 +0200

    ADD: ServiceListItem

---
+++ src/components/ServiceListItem/ServiceListItem.tsx
@@ -0,0 +1,88 @@
+import { FC } from "react";
+import { Box, IconButton, Typography, Grid } from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { motion } from "framer-motion";
+import { IModalImage, IPhoto, IService } from "../../types/types";
+
+interface IServiceListItem {
+  service: IService;
+  handleOpenDeletePhotoDialog: (photo: IPhoto) => void;
+  isLogedIn: boolean;
+  openImage: (image: IModalImage) => void;
+  index: number;
+}
+
+const ServiceListItem: FC<IServiceListItem> = ({
+  service,
+  handleOpenDeletePhotoDialog,
+  isLogedIn,
+  openImage,
+  index,
+}) => {
+  return (
+    <Grid
+      item
+      key={service.id}
+      maxWidth={250}
+      borderRadius={10}
+      overflow="hidden"
+      position="relative"
+      border={1}
+    >
+      <motion.img
+        src={service.url}
+        alt="portfolio pic"
+        loading="lazy"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
+        onClick={() =>
+          openImage({ index: index, url: service.url, id: service.id })
+        }
+      />
+      <Box
+        bgcolor="#00000080"
+        position="absolute"
+        margin="0 5%"
+        bottom="10px"
+        width="90%"
+        borderRadius={3}
+      >
+        <Typography variant="h6" color="#FFF" padding={1}>
+          {service.name}
+        </Typography>
+      </Box>
+      {isLogedIn && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1 }}
+          style={{
+            position: "absolute",
+            top: 5,
+            right: 5,
+            color: "red",
+            cursor: "pointer",
+          }}
+        >
+          <IconButton
+            sx={{
+              padding: 0.5,
+              margin: 0.5,
+              cursor: "pointer",
+              borderRadius: 5,
+              border: "1px solid #FFF",
+            }}
+            onClick={() => handleOpenDeletePhotoDialog(service)}
+          >
+            <DeleteOutlineIcon color="error" />
+          </IconButton>
+        </motion.div>
+      )}
+    </Grid>
+  );
+};
+
+export default ServiceListItem;
--- src/components/ServicesList/ServicesList.tsx
+++ src/components/ServicesList/ServicesList.tsx
@@ -1,7 +1,5 @@
 import { FC, useState } from "react";
-import { Box, IconButton, Typography, Grid } from "@mui/material";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import { motion } from "framer-motion";
+import { Grid } from "@mui/material";
 import deleteFile from "../../utils/deleteFile";
 import {
   CollectionTypes,
@@ -10,6 +8,7 @@ import {
   IService,
 } from "../../types/types";
 import ConfirmationDialog from "../ConfirmationDialog/ConfirmationDialog";
+import ServiceListItem from "../ServiceListItem/ServiceListItem";
 
 interface IMasonryImageList {
   list: IService[];
@@ -54,68 +53,13 @@ const ServiceList: FC<IMasonryImageList> = ({
     >
       {list &&
         list.map((item, index) => (
-          <Grid
-            item
-            key={item.id}
-            maxWidth={250}
-            borderRadius={10}
-            overflow="hidden"
-            position="relative"
-            border={1}
-          >
-            <motion.img
-              src={item.url}
-              alt="portfolio pic"
-              loading="lazy"
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-              }}
-              onClick={() =>
-                openImage({ index: index, url: item.url, id: item.id })
-              }
-            />
-            <Box
-              bgcolor="#00000080"
-              position="absolute"
-              margin="0 5%"
-              bottom="10px"
-              width="90%"
-              borderRadius={3}
-            >
-              <Typography variant="h6" color="#FFF" padding={1}>
-                {item.name}
-              </Typography>
-            </Box>
-            {isLogedIn && (
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 1 }}
-                style={{
-                  position: "absolute",
-                  top: 5,
-                  right: 5,
-                  color: "red",
-                  cursor: "pointer",
-                }}
-              >
-                <IconButton
-                  sx={{
-                    padding: 0.5,
-                    margin: 0.5,
-                    cursor: "pointer",
-                    borderRadius: 5,
-                    border: "1px solid #FFF",
-                  }}
-                  onClick={() => handleOpenDeletePhotoDialog(item)}
-                >
-                  <DeleteOutlineIcon color="error" />
-                </IconButton>
-              </motion.div>
-            )}
-          </Grid>
+          <ServiceListItem
+            service={item}
+            handleOpenDeletePhotoDialog={handleOpenDeletePhotoDialog}
+            isLogedIn={isLogedIn}
+            index={index}
+            openImage={openImage}
+          />
         ))}
       <ConfirmationDialog
         open={openDeletePhotoDialog}
