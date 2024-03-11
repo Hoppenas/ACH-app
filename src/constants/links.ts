5caa80486d238587d12b22c928e41125744ae4a8
Martynas Hoppenas <martynas.hoppenas@gmail.com>
2024-03-11 18:31:58 +0200

    UPD: fb name

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,10 +1,10 @@
 export const instagramLink = "https://www.instagram.com/agne_grimas/";
 
-export const facebookLink = "https://www.facebook.com/agne.chaaa/";
+export const facebookLink = "https://www.facebook.com/agne.grimas/";
 
 export const instagramName = "agne_grimas";
 
-export const facebookName = "agne.chaaa";
+export const facebookName = "agne.grimas";
 
 export const tel = "+370 611 34488";
 
